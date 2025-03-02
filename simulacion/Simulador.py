153374e6ee28d3b3633bb7bc9c2f26da12c11182
silversitoooo <164232812+silversitoooo@users.noreply.github.com>
2025-03-01 20:29:44 -0600

    simulador

---
+++ simulacion/Simulador.py
@@ -0,0 +1,34 @@
+import simpy
+import random
+from .gestor_memoria import GestorMemoria
+from .procesador import Procesador
+
+class Simulador:
+    def __init__(self, env, num_procesos, capacidad_memoria, num_nucleos):
+        """Inicializa la simulación con memoria y procesador."""
+        self.env = env
+        self.memoria = GestorMemoria(env, capacidad_memoria)
+        self.procesador = Procesador(env, num_nucleos)
+        self.num_procesos = num_procesos
+
+    def proceso(self, id_proceso, tiempo_uso, memoria_requerida):
+        """Simula la ejecución de un proceso."""
+        print(f'[{self.env.now}] Proceso {id_proceso} esperando memoria...')
+        yield self.memoria.solicitar_memoria(memoria_requerida)
+
+        print(f'[{self.env.now}] Proceso {id_proceso} obtuvo memoria y espera CPU.')
+        with self.procesador.cpu.request() as req:
+            yield req
+            print(f'[{self.env.now}] Proceso {id_proceso} ejecutándose...')
+            yield self.env.timeout(tiempo_uso)
+
+        print(f'[{self.env.now}] Proceso {id_proceso} finalizado, liberando memoria.')
+        yield self.memoria.liberar_memoria(memoria_requerida)
+
+    def correr(self):
+        """Crea y ejecuta los procesos en el entorno de simulación."""
+        for i in range(self.num_procesos):
+            tiempo_uso = random.expovariate(1.0 / 10)
+            memoria_requerida = random.randint(1, 10)
+            self.env.process(self.proceso(i, tiempo_uso, memoria_requerida))
+        self.env.run()
